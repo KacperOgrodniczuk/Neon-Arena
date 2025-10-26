990da265333f23c5f53500578fef9a302bd846ae
KacperOgrodniczuk <85175239+KacperOgrodniczuk@users.noreply.github.com>
2025-10-26 23:12:50 +0000

    Fixed function call

--- Assets/Scripts/Player Scripts/PlayerHealthManager.cs
+++ Assets/Scripts/Player Scripts/PlayerHealthManager.cs
@@ -79,7 +79,7 @@ public class PlayerHealthManager : NetworkBehaviour
         currentHealth.Value = maxHealth;
         isDead.Value = false;
 
-        Transform spawnPoint = SpawnManager.Instance.GetRandomSpawnPoint(this.NetworkObject);
+        Transform spawnPoint = SpawnManager.Instance.GetLeastBusySpawnPoint(this.NetworkObject);
 
         RespawnPlayerRPC(Owner, spawnPoint.position, spawnPoint.rotation);
     }
